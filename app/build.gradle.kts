a33f8c79c07cfc40cb8746e371750a2876e11699
alekseypavlov345-boop <alekseypavlov345@gmail.com>
2026-08-12 22:16:20 +0900

    Add app Gradle build

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.rknshield.android"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.rknshield.android"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 2
+        versionName = "2.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.wireguard.android:tunnel:1.0.20260315")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
